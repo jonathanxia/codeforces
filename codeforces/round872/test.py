73e64bc70c7387b7ef879fbd776c940354a4d415
Jonathan Xia <jxia934@gmail.com>
2023-05-14 00:32:00 -0400

    Add commands

--- codeforces/round872/test.py
+++ codeforces/round872/test.py
@@ -1,3 +1,8 @@
-print(78329,2)
-for i in range(78328):
-    print(i+2,min(i+1,58986))
+import numpy as np
+N = 10 ** 4
+print(N)
+print(" ".join([str(s) for s in np.random.randint(0, 10 ** 9 - 1, size=(N,))]))
+
+for i in range(N // 2):
+    print(2 * i + 1, 2 * i + 2)
+    print(2 * i + 1, 2 * i + 3)
