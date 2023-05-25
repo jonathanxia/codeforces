73872079c0eee2e0ea5bcc5d3b2a4b731092724e
Jonathan Xia <jxia934@gmail.com>
2023-05-25 02:53:08 -0400

    Did some atcoder problem

--- atcoder/round302/ex.cpp
+++ atcoder/round302/ex.cpp
@@ -638,3 +638,161 @@ std::ostream& operator<<(std::ostream& os, const ndarray<T>& arr) {
 typedef ndarray<ll> llarray;
 typedef ndarray<int> intarray;
 
+class PersistantVector {
+public:
+    vl data;
+    stack<umapll> snapshots;
+
+    PersistantVector(int size) : data(size, 0) {
+        snapshots.push(umapll());
+    }
+
+    const ll operator[](int idx) const {
+        return data[idx];
+    }
+
+    void set(int idx, ll value) {
+        auto& ss = snapshots.top();
+        if (!ss.count(idx)) {
+            ss[idx] = data[idx];
+        }
+
+        data[idx] = value;
+    }
+
+    void commit() {
+        snapshots.push(umapll());
+    }
+
+    void revert() {
+        auto dd = snapshots.top();
+        snapshots.pop();
+
+        foreach(k, dd) {
+            data[k.first] = k.second;
+        }
+    }
+};
+
+class DSU {
+public:
+    PersistantVector parent;
+    PersistantVector rank;
+    PersistantVector is_alive;
+    ll num_alive;
+    stack<ll> num_alive_snaps;
+
+    DSU(int size) : parent(size), rank(size), is_alive(size) {
+        for (int i = 0; i < size; ++i) {
+            parent.set(i, i);
+            is_alive.set(i, true);
+            rank.set(i, 0);
+        }
+        num_alive = size;
+    }
+
+    int find(int x) {
+        if (parent[x] != x) {
+            parent.set(x, find(parent[x])); // Path compression
+        }
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] < rank[rootY]) {
+                parent.set(rootX, rootY);
+            } else if (rank[rootX] > rank[rootY]) {
+                parent.set(rootY, rootX);
+            } else {
+                parent.set(rootY, rootX);
+                rank.set(rootX, rank[rootX] + 1);
+            }
+
+            // Are we killing any components? Death is
+            // infectious
+            bool new_is_alive = is_alive[rootX] && is_alive[rootY];
+
+            num_alive = num_alive - is_alive[rootX] - is_alive[rootY] + new_is_alive;
+
+            is_alive.set(rootX, new_is_alive);
+            is_alive.set(rootY, new_is_alive);
+        }
+
+        else {
+            // Same connected component, if it was alive,
+            // it is now dead
+            if (is_alive[rootX]) {
+                num_alive--;
+                is_alive.set(rootX, false);
+            }
+        }
+
+    }
+
+    void commit() {
+        parent.commit();
+        rank.commit();
+        is_alive.commit();
+        num_alive_snaps.push(num_alive);
+    }
+
+    void revert() {
+        parent.revert();
+        rank.revert();
+        is_alive.revert();
+        num_alive = num_alive_snaps.top();
+        num_alive_snaps.pop();
+    }
+};
+
+
+
+void solve(vl& a, vl& b, vvl& graph, ll n) {
+    vl output(n);
+    DSU mydsu(n);
+
+    auto dfs = [&](auto&& self, ll node, ll parent) -> void {
+        mydsu.commit();
+        mydsu.unite(a[node], b[node]); 
+        output[node] = n - mydsu.num_alive;
+        foreach(child, graph[node]) {
+            if (child != parent) {
+                self(self, child, node);
+            }
+        }
+        mydsu.revert();
+    };
+
+    dfs(dfs, 0, -1);
+
+    print(str_join(vec::slice(output, 1, n), " "));
+}
+
+int main() {
+
+    ll n;
+    cin >> n;
+
+    vl a(n);
+    vl b(n);
+
+    vvl graph(n);
+
+    rep(i, 0, n - 1) {
+        cin >> a[i] >> b[i];
+    }
+
+    rep(i, 0, n - 2) {
+        ll u, v;
+        cin >> u >> v;
+        graph[u - 1].pb(v - 1);
+        graph[v - 1].pb(u - 1);
+    }
+
+    solve(a, b, graph, n);
+
+    return 0;
+}
