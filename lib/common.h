be718fd4190d0315ef10690358f82695b58e57c2
Kevin1729 <kxia1729@gmail.com>
2023-07-20 21:57:54 -0700

    Redo deduction of LC macro

--- lib/common.h
+++ lib/common.h
@@ -546,13 +546,16 @@ typedef priority_queue<ll, vl, less<ll>> maxheap;
     lcret;                       \
 })
 
-#define LC(expr, x, arr) ({                 \
-    using LC_t = decltype(arr)::value_type; \
-    vector<LC_t> lcret;                     \
-    foreach (x, arr) {                      \
-        lcret.push_back(expr);              \
-    }                                       \
-    lcret;                                  \
+#define LC(expr, x, arr) ({                                 \
+    using LCx_t = decltype(arr)::value_type;                \
+    LCx_t x;                                                \
+    using LCexpr_t = decltype(expr);                        \
+    vector<LCexpr_t> lcret;                                 \
+    foreach (i, arr) {                                      \
+        x = i; /*Using i and then x = i pacifies -Wshadow*/ \
+        lcret.push_back(expr);                              \
+    }                                                       \
+    lcret;                                                  \
 })
 
 int dx[4] = { 1, 0, -1, 0 };
