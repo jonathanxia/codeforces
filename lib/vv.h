15e110e646afdef977294f8d239057dda159d69e
jonathanxia <jxia934@gmail.com>
2023-07-22 10:23:22 -0400

    Make arange inline

--- lib/vv.h
+++ lib/vv.h
@@ -26,7 +26,7 @@ namespace vv {
     }
 
     template <typename T>
-    static vector<T> arange(T start, T end, T step = 1) {
+    inline vector<T> arange(T start, T end, T step = 1) {
         vector<T> result;
         for (T value = start; value <= end; value += step) {
             result.pb(value);
