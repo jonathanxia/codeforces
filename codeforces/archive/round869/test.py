8f4e05d286f38ca7b6769496cb4efb0b31e7e5f9
jonathanxia <jxia934@gmail.com>
2023-08-01 23:45:40 -0400

    Remove codeforces

--- codeforces/archive/round869/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-N = 2500000
-a = np.random.randint(0, 10 ** 9, N)
-b = np.random.randint(0, 10 ** 9, N)
-
-print(N - 1)
-print(" ".join([str(s) for s in a]))
-print(" ".join([str(s) for s in b]))
--- codeforces/archive/round879/rabbit.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-N = 300000
-print(1)
-print(N)
-print(" ".join([str(s) for s in np.random.randint(1, 3, N - 114738)] + list(map(str, range(1, 114739)))))
