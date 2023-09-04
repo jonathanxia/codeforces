d2515d6676c5935ef7d8143a3df06b1fd4fec292
jonathanxia <jxia934@gmail.com>
2023-09-04 13:28:43 -0400

    preprocess smarter by checking comments

--- preprocess.py
+++ preprocess.py
@@ -5,22 +5,36 @@ import re
 
 included_fnames = set()
 
-def open_filename(fname):
+REG = r"#include\s*<(lib/.*\.(h|py))"
+
+def open_filename(fname, comment):
     output = []
     with open(fname, "r") as f:
         for line in f.readlines():
             line = line.rstrip()
-            mm = re.match(r"#include\s*<(lib/.*\.(h|py))", line)
-            if not mm:
+
+            mm = re.match("^" + REG, line)
+            mm_comment = re.match(comment + r"\s*" + REG, line)
+
+            # If we had already imported it, don't import it again,
+            # but mark that we have imported it to avoid double
+            # imports
+            if mm_comment is not None:
+                incl_fn = mm_comment.group(1)
+                included_fnames.add(incl_fn)
+                continue
+        
+            if mm is not None:
+                incl_fn = mm.group(1)
+                if incl_fn in included_fnames:
+                    continue
+
+                output.extend(open_filename(incl_fn, comment))
+                included_fnames.add(incl_fn)
+
+            else:
                 output.append(line)
                 continue
-                
-            incl_fn = mm.group(1)
-            if incl_fn in included_fnames:
-                continue
-
-            output.extend(open_filename(incl_fn))
-            included_fnames.add(incl_fn)
 
     return output
 
@@ -31,7 +45,7 @@ if __name__ == "__main__":
         comment = "#"
     else:
         comment = "//"
-    out = "\n".join(open_filename(fname))
+    out = "\n".join(open_filename(fname, comment))
     preamble = "\n".join(
         [f"{comment} #include<{fname}>" for fname in included_fnames]
     )
