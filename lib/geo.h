3208db5c69e823db14d40acbcbd3f2dea86f7c2c
Kevin1729 <kxia1729@gmail.com>
2023-09-06 01:08:22 -0700

    geo: Add Point struct, convex_hull using Points, and polygon_perimeter using Points. Pulled from kactl.

--- lib/geo.h
+++ lib/geo.h
@@ -36,4 +36,79 @@ vector<pair<T, T>> convex_hull(vector<pair<T, T>>& points, bool top)
     }
     return ret;
 }
+template <class T>
+int sgn(T x) { return (x > 0) - (x < 0); }
+template <class T>
+struct Point {
+    typedef Point P;
+    T x, y;
+    explicit Point(T x_ = 0, T y_ = 0)
+        : x(x_)
+        , y(y_)
+    {
+    }
+    bool operator<(P p) const { return tie(x, y) < tie(p.x, p.y); }
+    bool operator==(P p) const { return tie(x, y) == tie(p.x, p.y); }
+    P operator+(P p) const { return P(x + p.x, y + p.y); }
+    P operator-(P p) const { return P(x - p.x, y - p.y); }
+    P operator*(T d) const { return P(x * d, y * d); }
+    P operator/(T d) const { return P(x / d, y / d); }
+    T dot(P p) const { return x * p.x + y * p.y; }
+    T cross(P p) const { return x * p.y - y * p.x; }
+    T cross(P a, P b) const { return (a - *this).cross(b - *this); }
+    T dist2() const { return x * x + y * y; }
+    double dist() const { return sqrt((double)dist2()); }
+    // angle to x-axis in interval [-pi, pi]
+    double angle() const { return atan2(y, x); }
+    P unit() const { return *this / dist(); } // makes dist()=1
+    P perp() const { return P(-y, x); } // rotates +90 degrees
+    P normal() const { return perp().unit(); }
+    // returns point rotated 'a' radians ccw around the origin
+    P rotate(double a) const
+    {
+        return P(x * cos(a) - y * sin(a), x * sin(a) + y * cos(a));
+    }
+    friend ostream& operator<<(ostream& os, P p)
+    {
+#ifndef ONLINE_JUDGE
+#ifdef DEBUG
+        return os << "(" << p.x << "," << p.y << ")";
+#endif
+#endif
+        return os << p.x << p.y;
+    }
+    friend istream& operator>>(istream& is, P& p)
+    {
+        is >> p.x >> p.y;
+        return is;
+    }
+};
+template <typename T>
+vector<Point<T>> convex_hull(vector<Point<T>> pts)
+{
+    if (len(pts) <= 1)
+        return pts;
+    sort(ALL(pts));
+    vector<Point<T>> h(len(pts) + 1);
+    int s = 0, t = 0;
+    for (int it = 2; it--; s = --t, reverse(ALL(pts)))
+        for (Point<T> p : pts) {
+            while (t >= s + 2 && h[t - 2].cross(h[t - 1], p) <= 0)
+                t--;
+            h[t++] = p;
+        }
+    return { h.begin(), h.begin() + t - (t == 2 && h[0] == h[1]) };
+}
+template <typename T>
+vector<Point<T>> polygon_perimeter(vector<Point<T>> pts)
+{
+    ll n = len(pts);
+    T perimeter = 0;
+    walk(i, pts)
+    {
+        Point<T> displacement = pts[i] - pts[mod(i - 1, n)];
+        perimeter += displacement.dist();
+    }
+    return perimeter;
+}
 }
