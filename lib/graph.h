b9bcf8c0d222de10e74fec6fa4da38c539dca8e9
Kevin1729 <kxia1729@gmail.com>
2023-09-06 15:10:20 -0700

    graph: implement floyd_warshall(). Pulled from kactl.

--- lib/graph.h
+++ lib/graph.h
@@ -131,6 +131,25 @@ namespace graph
         reverse(order.begin(), order.end());
         return order;
     }
+    const ll inf = 1LL << 62;
+    // Input: a matrix m with:
+    // m[i][i] = 0 for all i
+    // m[i][j] = weight(i, j) for adjecent nodes i, j
+    // m[i][j] = graph::inf for non-adjacent nodes i, j
+    void floyd_warshall(vvl& m)
+    {
+        int n = len(m);
+        repe(i, 0, n) m[i][i] = min(m[i][i], 0LL);
+        repe(k, 0, n) repe(i, 0, n) repe(j, 0, n) if (m[i][k] != inf && m[k][j] != inf)
+        {
+            auto newDist = max(m[i][k] + m[k][j], -inf);
+            m[i][j] = min(m[i][j], newDist);
+        }
+        rep(k, 0, n) if (m[k][k] < 0) rep(i, 0, n) rep(j, 0, n) if (m[i][k] != inf && m[k][j] != inf) m[i][j] = -inf;
+    }
+
+///   TREE  ALGORITHMS   ///
+/// Also check out dfs.h ///
 
     // Only works for trees!
     vl longest_path(vvl &graph, ll n)
@@ -168,4 +187,7 @@ namespace graph
 
         return path;
     }
+
+///   TREE  ALGORITHMS   ///
+/// Also check out dfs.h ///
 }
