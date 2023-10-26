93852ec1f1fca52eec1aa8ac7a6e7a9b9717a66a
Kevin1729 <kxia1729@gmail.com>
2023-10-25 21:34:59 -0700

    fft: make relevant int to ll changes, renamed convMod to conv_mod

--- lib/fft.h
+++ lib/fft.h
@@ -1,96 +1,77 @@
 #include <lib/common.h>
 
 namespace fft {
-    template <typename T>
-    inline int sz(T x) { return len(x); }
-/**
- * Author: Ludo Pulles, chilli, Simon Lindholm
- * Date: 2019-01-09
- * License: CC0
- * Source: http://neerc.ifmo.ru/trains/toulouse/2017/fft2.pdf (do read, it's excellent)
-   Accuracy bound from http://www.daemonology.net/papers/fft.pdf
- * Description: fft(a) computes $\hat f(k) = \sum_x a[x] \exp(2\pi i \cdot k x / N)$ for all $k$. N must be a power of 2.
-   Useful for convolution:
-   \texttt{conv(a, b) = c}, where $c[x] = \sum a[i]b[x-i]$.
-   For convolution of complex numbers or more than two vectors: FFT, multiply
-   pointwise, divide by n, reverse(start+1, end), FFT back.
-   Rounding is safe if $(\sum a_i^2 + \sum b_i^2)\log_2{N} < 9\cdot10^{14}$
-   (in practice $10^{16}$; higher for random inputs).
-   Otherwise, use NTT/FFTMod.
- * Time: O(N \log N) with $N = |A|+|B|$ ($\tilde 1s$ for $N=2^{22}$)
- * Status: somewhat tested
- * Details: An in-depth examination of precision for both FFT and FFTMod can be found
- * here (https://github.com/simonlindholm/fft-precision/blob/master/fft-precision.md)
- */
+#define sz len
 typedef complex<double> C;
-void fft(vector<C>& a) {
-	int n = len(a), L = 31 - __builtin_clz(n);
-	static vector<complex<long double>> R(2, 1);
-	static vector<C> rt(2, 1);  // (^ 10% faster if double)
-	for (static int k = 2; k < n; k *= 2) {
-		R.resize(n); rt.resize(n);
-		auto x = polar(1.0L, acos(-1.0L) / k);
-		rep(i,k,2*k) rt[i] = R[i] = i&1 ? R[i/2] * x : R[i/2];
-	}
-	vi rev(n);
-	rep(i,0,n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
-	rep(i,0,n) if (i < rev[i]) swap(a[i], a[rev[i]]);
-	for (int k = 1; k < n; k *= 2)
-		for (int i = 0; i < n; i += 2 * k) rep(j,0,k) {
-			// C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
-			auto x = (double *)&rt[j+k], y = (double *)&a[i+j+k];        /// exclude-line
-			C z(x[0]*y[0] - x[1]*y[1], x[0]*y[1] + x[1]*y[0]);           /// exclude-line
-			a[i + j + k] = a[i + j] - z;
-			a[i + j] += z;
-		}
+typedef vector<long double> vd;
+void fft(vector<C>& a)
+{
+    ll n = sz(a), L = 31 - __builtin_clz(n);
+    static vector<complex<long double>> R(2, 1);
+    static vector<C> rt(2, 1); // (^ 10% faster if double)
+    for (static int k = 2; k < n; k *= 2) {
+        R.resize(n);
+        rt.resize(n);
+        auto x = polar(1.0L, acos(-1.0L) / k);
+        rep(i, k, 2 * k) rt[i] = R[i] = i & 1 ? R[i / 2] * x : R[i / 2];
+    }
+    vi rev(n);
+    rep(i, 0, n) rev[i] = (rev[i / 2] | (i & 1) << L) / 2;
+    rep(i, 0, n) if (i < rev[i]) swap(a[i], a[rev[i]]);
+    for (int k = 1; k < n; k *= 2)
+        for (int i = 0; i < n; i += 2 * k)
+            rep(j, 0, k)
+            {
+                // C z = rt[j+k] * a[i+j+k]; // (25% faster if hand-rolled)  /// include-line
+                auto x = (double*)&rt[j + k], y = (double*)&a[i + j + k]; /// exclude-line
+                C z(x[0] * y[0] - x[1] * y[1], x[0] * y[1] + x[1] * y[0]); /// exclude-line
+                a[i + j + k] = a[i + j] - z;
+                a[i + j] += z;
+            }
 }
-vd conv(const vd& a, const vd& b) {
-	if (a.empty() || b.empty()) return {};
-	vd res(len(a) + len(b) - 1);
-	int L = 32 - __builtin_clz(len(res)), n = 1 << L;
-	vector<C> in(n), out(n);
-	copy(ALL(a), begin(in));
-	rep(i,0,len(b)) in[i].imag(b[i]);
-	fft(in);
-	for (C& x : in) x *= x;
-	rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
-	fft(out);
-	rep(i,0,len(res)) res[i] = imag(out[i]) / (4 * n);
-	return res;
+vd conv(const vd& a, const vd& b)
+{
+    if (a.empty() || b.empty())
+        return {};
+    vd res(sz(a) + sz(b) - 1);
+    ll L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+    vector<C> in(n), out(n);
+    copy(ALL(a), in.begin());
+    rep(i, 0, sz(b)) in[i].imag(b[i]);
+    fft(in);
+    for (C& x : in)
+        x *= x;
+    rep(i, 0, n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+    fft(out);
+    rep(i, 0, sz(res)) res[i] = imag(out[i]) / (4 * n);
+    return res;
 }
 
-/**
- * Author: chilli
- * Date: 2019-04-25
- * License: CC0
- * Source: http://neerc.ifmo.ru/trains/toulouse/2017/fft2.pdf
- * Description: Higher precision FFT, can be used for convolutions modulo arbitrary integers
- * as long as $N\log_2N\cdot \text{mod} < 8.6 \cdot 10^{14}$ (in practice $10^{16}$ or higher).
- * Inputs must be in $[0, \text{mod})$.
- * Time: O(N \log N), where $N = |A|+|B|$ (twice as slow as NTT or FFT)
- * Status: stress-tested
- * Details: An in-depth examination of precision for both FFT and FFTMod can be found
- * here (https://github.com/simonlindholm/fft-precision/blob/master/fft-precision.md)
- */
-template<int M=MOD> vl convMod(const vl &a, const vl &b) {
-	if (a.empty() || b.empty()) return {};
-	vl res(len(a) + len(b) - 1);
-	int B=32-__builtin_clz(sz(res)), n=1<<B, cut=int(sqrt(M));
-	vector<C> L(n), R(n), outs(n), outl(n);
-	rep(i,0,sz(a)) L[i] = C((int)a[i] / cut, (int)a[i] % cut);
-	rep(i,0,sz(b)) R[i] = C((int)b[i] / cut, (int)b[i] % cut);
-	fft(L), fft(R);
-	rep(i,0,n) {
-		int j = -i & (n - 1);
-		outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
-		outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
-	}
-	fft(outl), fft(outs);
-	rep(i,0,sz(res)) {
-		ll av = ll(real(outl[i])+.5), cv = ll(imag(outs[i])+.5);
-		ll bv = ll(imag(outl[i])+.5) + ll(real(outs[i])+.5);
-		res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
-	}
-	return res;
+typedef vector<ll> vl;
+template <ll M = MOD>
+vl conv_mod(const vl& a, const vl& b)
+{
+    if (a.empty() || b.empty())
+        return {};
+    vl res(sz(a) + sz(b) - 1);
+    ll B = 32 - __builtin_clz(sz(res)), n = 1 << B, cut = ll(sqrt(M));
+    vector<C> L(n), R(n), outs(n), outl(n);
+    rep(i, 0, sz(a)) L[i] = C((ll)a[i] / cut, (ll)a[i] % cut);
+    rep(i, 0, sz(b)) R[i] = C((ll)b[i] / cut, (ll)b[i] % cut);
+    fft(L), fft(R);
+    rep(i, 0, n)
+    {
+        ll j = -i & (n - 1);
+        outl[j] = (L[i] + conj(L[j])) * R[i] / (2.0 * n);
+        outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
+    }
+    fft(outl), fft(outs);
+    rep(i, 0, sz(res))
+    {
+        ll av = ll(real(outl[i]) + .5), cv = ll(imag(outs[i]) + .5);
+        ll bv = ll(imag(outl[i]) + .5) + ll(real(outs[i]) + .5);
+        res[i] = ((av % M * cut + bv) % M * cut + cv) % M;
+    }
+    return res;
 }
 }
