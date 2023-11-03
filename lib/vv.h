81b3a916dc082ec28c9256eceb4345b9ecc68078
jonathanxia <jxia934@gmail.com>
2023-11-03 14:39:04 +0900

    vv: sort for coord compress

--- lib/vv.h
+++ lib/vv.h
@@ -415,9 +415,11 @@ template <typename T>
 vector<ll> coord_compress(const vector<T>& a, bool dedupe = true) {
     if(dedupe)
     {
+        vi s_idx = argsort(a);
         umap<T, ll> coord;
         ll idx = 0;
-        foreach(x, a) {
+        foreach(xi, s_idx) {
+            T x = a[xi];
             if (coord.count(x) == 0) {
                 coord[x] = idx;
                 idx++;
