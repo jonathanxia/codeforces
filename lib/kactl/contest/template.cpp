a1879758a38f5e879b76719d50eace848b9dee36
jonathanxia <jxia934@gmail.com>
2023-11-13 00:58:02 +0900

    Import kactl library

---
+++ lib/kactl/contest/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define ALL(x) begin(x), end(x)
+#define len(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
+}
---
+++ lib/kactl/various/LIS.h
@@ -0,0 +1,26 @@
+/**
+ * Author: Johan Sannemo
+ * License: CC0
+ * Description: Compute indices for the longest increasing subsequence.
+ * Time: $O(N \log N)$
+ * Status: Tested on kattis:longincsubseq, stress-tested
+ */
+#pragma once
+
+template<class I> vi lis(const vector<I>& S) {
+	if (S.empty()) return {};
+	vi prev(len(S));
+	typedef pair<I, int> p;
+	vector<p> res;
+	rep(i,0,len(S)) {
+		// change 0 -> i for longest non-decreasing subsequence
+		auto it = lower_bound(ALL(res), p{S[i], 0});
+		if (it == res.end()) res.emplace_back(), it = res.end()-1;
+		*it = {S[i], i};
+		prev[i] = it == res.begin() ? 0 : (it-1)->second;
+	}
+	int L = len(res), cur = res.back().second;
+	vi ans(L);
+	while (L--) ans[L] = cur, cur = prev[cur];
+	return ans;
+}
