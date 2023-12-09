f1717f924ec03d0fae169ffe3115d82b90fd6466
jonathanxia <jxia934@gmail.com>
2023-12-08 19:42:56 -0500

    vv.h: add mex()

--- lib/vv.h
+++ lib/vv.h
@@ -436,5 +436,19 @@ vector<ll> coord_compress(const vector<T>& a, bool dedupe = true) {
         return to_vl(argsort(argsort(a)));
     }
 }
-};
 
+template <typename T>
+ll mex(const vector<T>& a, bool positive=false) {
+    ll n = len(a);
+    ll start = int(positive);
+    vb exists(start + n);
+    FOR(i, 0, n - 1) {
+        if (a[i] >= n + start) continue;
+        exists[a[i]] = true;
+    }
+    FOR(i, start, n - 1 + start) {
+        if (!exists[i]) return i;
+    }
+    return n + start;
+}
+}; // namespace vv
