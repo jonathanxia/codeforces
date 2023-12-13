df9db6084a144f1618223e8246bc662e8964dc7d
jonathanxia <jxia934@gmail.com>
2023-12-12 23:19:49 -0500

    subset_sum.h: Add version that returns actual subset

--- lib/subset_sum.h
+++ lib/subset_sum.h
@@ -18,3 +18,40 @@ bool subset_sum(const vl& items, int target)
 
     return dp[target];
 }
+
+/*
+* Actually provides which subset will achieve the target sum
+*/
+bool slow_subset_sum_with_sol(const vl& items, ll target, vb& marked) {
+    ll m = len(items);
+    ll tot = 0;
+    FOR(i, 0, m - 1) tot += items[i];
+    marked.resize(m);
+    FOR(i, 0, m - 1) marked[i] = false;
+
+    if (target > tot) return false;
+
+    vector<vb> dp(tot + 1, vb(m + 1));
+    dp[0][0] = true;
+
+    FOR(j, 1, m) {
+        FOR(s, 0, tot) {
+            dp[s][j] = dp[s][j - 1];
+            if (items[j - 1] <= s) {
+                dp[s][j] = dp[s][j] || dp[s - items[j - 1]][j - 1];
+            }
+        }
+    }
+
+    if (!dp[target][m]) return false;
+
+    DOR(j, m, 1) {
+        ll elem = items[j - 1];
+        if (elem <= target && dp[target - elem][j - 1]) {
+            target -= elem;
+            marked[j - 1] = true;
+        }
+    }
+
+    return true;
+}
