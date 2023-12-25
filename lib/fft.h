9551ac6c6dcfe23180a942020ffea494adc8140d
Jonathan Xia <jxia934@gmail.com>
2023-12-25 15:54:44 -0500

    fft.h: Fix fft

--- lib/fft.h
+++ lib/fft.h
@@ -4,7 +4,7 @@ namespace fft {
 typedef complex<double> C;
 void fft(vector<C>& a)
 {
-    ll n = sz(a), L = 31 - __builtin_clz(n);
+    ll n = len(a), L = 31 - __builtin_clz(n);
     static vector<complex<long double>> R(2, 1);
     static vector<C> rt(2, 1); // (^ 10% faster if double)
     for (static int k = 2; k < n; k *= 2) {
@@ -31,17 +31,17 @@ vld conv(const vld& a, const vld& b)
 {
     if (a.empty() || b.empty())
         return {};
-    vld res(sz(a) + sz(b) - 1);
-    ll L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+    vld res(len(a) + len(b) - 1);
+    ll L = 32 - __builtin_clz(len(res)), n = 1 << L;
     vector<C> in(n), out(n);
     copy(ALL(a), in.begin());
-    rep(i, 0, sz(b)) in[i].imag(b[i]);
+    rep(i, 0, len(b)) in[i].imag(b[i]);
     fft(in);
     for (C& x : in)
         x *= x;
     rep(i, 0, n) out[i] = in[-i & (n - 1)] - conj(in[i]);
     fft(out);
-    rep(i, 0, sz(res)) res[i] = imag(out[i]) / (4 * n);
+    rep(i, 0, len(res)) res[i] = imag(out[i]) / (4 * n);
     return res;
 }
 
@@ -50,11 +50,11 @@ vl conv_mod(const vl& a, const vl& b)
 {
     if (a.empty() || b.empty())
         return {};
-    vl res(sz(a) + sz(b) - 1);
-    ll B = 32 - __builtin_clz(sz(res)), n = 1 << B, cut = ll(sqrt(M));
+    vl res(len(a) + len(b) - 1);
+    ll B = 32 - __builtin_clz(len(res)), n = 1 << B, cut = ll(sqrt(M));
     vector<C> L(n), R(n), outs(n), outl(n);
-    rep(i, 0, sz(a)) L[i] = C((ll)a[i] / cut, (ll)a[i] % cut);
-    rep(i, 0, sz(b)) R[i] = C((ll)b[i] / cut, (ll)b[i] % cut);
+    rep(i, 0, len(a)) L[i] = C((ll)a[i] / cut, (ll)a[i] % cut);
+    rep(i, 0, len(b)) R[i] = C((ll)b[i] / cut, (ll)b[i] % cut);
     fft(L), fft(R);
     rep(i, 0, n)
     {
@@ -63,7 +63,7 @@ vl conv_mod(const vl& a, const vl& b)
         outs[j] = (L[i] - conj(L[j])) * R[i] / (2.0 * n) / 1i;
     }
     fft(outl), fft(outs);
-    rep(i, 0, sz(res))
+    rep(i, 0, len(res))
     {
         ll av = ll(real(outl[i]) + .5), cv = ll(imag(outs[i]) + .5);
         ll bv = ll(imag(outl[i]) + .5) + ll(real(outs[i]) + .5);
