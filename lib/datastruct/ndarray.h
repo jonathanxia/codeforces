aae4d04026fc184ea59b952aa9ab8374b97147b1
Jonathan Xia <jxia934@gmail.com>
2024-02-17 16:53:49 -0500

    ndarray.h: Add wyatt's ndarray

---
+++ lib/datastruct/ndarray.h
@@ -0,0 +1,29 @@
+#include <lib/common.h>
+
+// arbitrarily high dimension array
+// by default will return a default_value on OOB access
+template<typename T, int num_dimensions>
+struct ndarray {
+    ndarray(array<int, num_dimensions> _dimensions, T default_value=0) {
+        dimensions = _dimensions;
+        int rolling_mult = 1;
+        DOR(i, num_dimensions-1, 0) {
+           multiplier[i] = rolling_mult;
+           rolling_mult *= dimensions[i];
+        }
+        data = vector<T>(rolling_mult + 1, default_value);
+    }
+
+    template <typename... Indices>
+    T& operator() (Indices... args) {
+        array<int, num_dimensions> indices = {static_cast<int>(args)...};
+        int flatIndex = 0;
+        FOR(i, 0, num_dimensions-1) if(indices[i] < 0 or indices[i] >= dimensions[i]) return data.back();
+        FOR(i, 0, num_dimensions-1) flatIndex += indices[i] * multiplier[i];
+        return data[flatIndex];
+    }
+
+    array<int, num_dimensions> multiplier;
+    array<int, num_dimensions> dimensions;
+    vector<T> data;
+};
