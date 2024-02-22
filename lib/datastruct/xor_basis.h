c6d40fcda2e01d60e4b39564d600b72b6c014966
Jonathan Xia <jxia934@gmail.com>
2024-02-21 22:51:08 -0500

    xor_basis.h: Add Rado's new XOR basis

---
+++ lib/datastruct/xor_basis.h
@@ -0,0 +1,84 @@
+#include <lib/common.h>
+
+/**
+ * Maintains a basis in XOR space. Given a bunch of integers
+ * a[0], a[1], ..., a[n], query what sorts of numbers
+ * can be expressed as an XOR of some subset.
+*/
+template<class T>
+struct basis {
+    int max_log;
+    vector<T> base;
+ 
+    void init(int _max_log) {
+        max_log = _max_log;
+        base.assign(max_log, 0);
+    }
+ 
+    void add(T val) {
+        for(int i = max_log - 1; i >= 0; i--) {
+            if((val >> i) & 1) {
+                if(!base[i]) {
+                    base[i] = val;
+                    return;
+                } else {
+                    val ^= base[i];
+                }
+            }
+        }
+    }
+ 
+    inline int size() {
+        int sz = 0;
+        for(int i = 0; i < max_log; i++) {
+            sz += (bool)(base[i]);
+        }
+        return sz;
+    }
+ 
+    T max_xor() {
+        T res = 0;
+        for(int i = max_log - 1; i >= 0; i--) {
+            if(!((res >> i) & 1) && base[i]) {
+                res ^= base[i];
+            }
+        }
+ 
+        return res;
+    }
+ 
+    bool can_create(T val) {
+        for(int i = max_log - 1; i >= 0; i--) {
+            if(((val >> i) & 1) && base[i]) {
+                val ^= base[i];
+            }
+        }
+ 
+        return (val == 0);
+    }
+ 
+    vector<T> get_basis() {
+        vector<T> res;
+        for(int i = 0; i < max_log; i++) {
+            if(base[i]) {
+                res.push_back(base[i]);
+            }
+        }
+        return res;
+    }
+ 
+    basis<T> merge(basis<T> other) {
+        if(max_log < other.max_log) {
+            return other.merge(*this);
+        }
+ 
+        basis<T> res = *this;
+        for(auto x: other.base) {
+            if(x) {
+                res.add(x);
+            }
+        }
+        return res;
+    }
+};
+ 
