9cc0c11023e9c1ee080574e9f3610f0f9966cc5f
Jonathan Xia <jxia934@gmail.com>
2024-05-08 23:11:53 -0400

    topk_tracker.h: Add version with counts

--- lib/datastruct/topk_tracker.h
+++ lib/datastruct/topk_tracker.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <lib/common.h>
 
+// You should use TopKTrackerWithCounts unless you are memory constrained
 template <typename T, std::size_t K>
 struct TopKTracker {
     std::array<T, K> topKElements;
@@ -41,5 +42,48 @@ struct TopKTracker {
 
     bool containsElement(T element) const {
         return std::find(topKElements.begin(), topKElements.begin() + size, element) != topKElements.begin() + size;
+    }
+};
+
+template <typename T, std::size_t K>
+struct TopKTrackerWithCounts {
+    // An array where each element is a pair {element, count}
+    // This array is sorted in decreasing order, so topKElements[0] is the largest
+    std::array<pair<T, ll>, K> topKElements;
+    int size;
+
+    TopKTrackerWithCounts() : size(0) {}
+
+    void addElement(T element, ll count) {
+        // Determine if element is inside
+        FOR(i, 0, size - 1) {
+            if (topKElements[i].first == element) {
+                topKElements[i].second += count;
+                return;
+            }
+        }
+        if (size < K) {
+            topKElements[size++] = mp(element, count);
+            manualInsertionSort();
+        } else if (element > topKElements[K - 1]) {
+            topKElements[K - 1] = mp(element, count);
+            manualInsertionSort();
+        }
+    }
+
+    TopKTracker operator+(const TopKTracker& other) const {
+        TopKTracker result = *this;
+
+        for (std::size_t i = 0; i < other.size; ++i) {
+            result.addElement(other.topKElements[i]);
+        }
+
+        return result;
+    }
+
+    void manualInsertionSort() {
+        for (std::size_t i = size - 1; i > 0 && topKElements[i] > topKElements[i - 1]; --i) {
+            std::swap(topKElements[i], topKElements[i - 1]);
+        }
     }
 };
