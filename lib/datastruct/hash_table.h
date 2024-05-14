e9734f3b881b90f2e24ebfd92b9322c30692a812
Jonathan Xia <jxia934@gmail.com>
2024-05-14 00:56:23 -0400

    Revert "hash_table.h: add faster (allegedly) hash table"
    This reverts commit 9d1de5e5268d8f7d43de25e41b3d869bf29962c7.

--- lib/datastruct/hash_table.h
+++
@@ -1,9 +0,0 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <lib/macros.h>
-
-// Fast umap and fast uset (fumap and fuset)
-template <typename K, typename V>
-using fumap = __gnu_pbds::gp_hash_table<K, V, custom_hash>;
-
-template <typename K>
-using fuset = __gnu_pbds::gp_hash_table<K, __gnu_pbds::null_type, custom_hash>;
