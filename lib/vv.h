8090bc96e139f54c24a44c1e2da4323aee8f5371
Jonathan Xia <jxia934@gmail.com>
2024-05-16 01:25:33 -0400

    vv.h: make sum better by returning 0

--- lib/vv.h
+++ lib/vv.h
@@ -102,6 +102,7 @@ T sum(const vector<T>& a, int start = 0, int end = -1)
     if (end < 0) {
         end = len(a) + end;
     }
+    if (start > end) return T(0);
     return std::accumulate(a.begin() + start, a.begin() + end + 1, T(0));
 }
 
