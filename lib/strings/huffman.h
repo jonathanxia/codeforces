6b9daafe3033974cbde7c3ad8601768817548d16
Jonathan Xia <jxia934@gmail.com>
2024-05-20 17:37:29 -0400

    huffman.h: Make it better

--- lib/strings/huffman.h
+++ lib/strings/huffman.h
@@ -9,20 +9,21 @@
  * using namespace huffman;
  * // build a node
  * vector<double> freq = {0.1, 0.2, 0.3};
- * HuffmanNode * node = buildHuffmanTree(freq);
+ * HuffmanTree tree(freq);
  * 
- * ...
- * delete node;
+ * tree.depth[1]
 */
 namespace huffman {
     // A Huffman tree node
     struct HuffmanNode {
         double freq;           // Frequency (probability) of the character
         HuffmanNode *left, *right; // Left and right child
+        ll id; // An id number, only valid for leaf nodes
 
-        HuffmanNode(double freq_) {
+        HuffmanNode(double freq_, ll id_) {
             left = right = nullptr;
             this->freq = freq_;
+            this->id = id_;
         }
         
         virtual ~HuffmanNode() {
@@ -41,23 +42,64 @@ namespace huffman {
     };
 
     // Function to build a Huffman Tree and return its root
-    HuffmanNode* buildHuffmanTree(const vector<double>& freq) {
-        priority_queue<HuffmanNode*, vector<HuffmanNode*>, compare> minHeap;
-        for (double f : freq)
-            minHeap.push(new HuffmanNode(f));
+    struct HuffmanTree {
+        HuffmanNode * root;
+        vl depth;
 
-        while (minHeap.size() != 1) {
-            HuffmanNode* left = minHeap.top(); minHeap.pop();
-            HuffmanNode* right = minHeap.top(); minHeap.pop();
-
-            HuffmanNode* top = new HuffmanNode(left->freq + right->freq);
-            top->left = left;
-            top->right = right;
-
-            minHeap.push(top);
+        HuffmanTree() {
+            root = nullptr;
         }
 
-        return minHeap.top();
-    }
+        // exclude_zero: toss out zero frequency symbols from consideration
+        HuffmanTree(const vector<double>& freq, bool exclude_zero=false) {
+            ll n = len(freq);
+            vl ids;
+            vector<double> freq_;
+            FOR(i, 0, n - 1) {
+                if (!exclude_zero || freq[i] > 0) {
+                    ids.pb(i);
+                    freq_.pb(freq[i]);
+                }
+            }
+            root = buildHuffmanTree(freq_, ids);
+            depth.resize(n);
+            computeDepths(root, 0);
+        }
+
+        HuffmanNode* buildHuffmanTree(const vector<double>& freq, const vl& ids) {
+            priority_queue<HuffmanNode*, vector<HuffmanNode*>, compare> minHeap;
+            walk(i, freq) {
+                minHeap.push(new HuffmanNode(freq[i], ids[i]));
+            }
+
+            while (minHeap.size() != 1) {
+                HuffmanNode* left = minHeap.top(); minHeap.pop();
+                HuffmanNode* right = minHeap.top(); minHeap.pop();
+
+                HuffmanNode* top = new HuffmanNode(left->freq + right->freq, -1);
+                top->left = left;
+                top->right = right;
+
+                minHeap.push(top);
+            }
+
+            return minHeap.top();
+        }
+
+        void computeDepths(HuffmanNode * node, ll d) {
+            if (node->left == nullptr) {
+                assert(node->right == nullptr);
+                depth[node->id] = d;
+            }
+            else {
+                computeDepths(node->left, d + 1);
+                computeDepths(node->right, d + 1);
+            }
+        }
+
+        ~HuffmanTree() {
+            delete root;
+        }
+    };
 } // namespace huffman
 
