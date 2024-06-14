c8d617dfa3fbad7373f9787fe0c9f22a2193337c
Jonathan Xia <jxia934@gmail.com>
2024-06-14 12:18:09 -0400

    ndarray.h: Make default value a separate field

--- lib/datastruct/ndarray.h
+++ lib/datastruct/ndarray.h
@@ -13,52 +13,57 @@
 */
 template<typename T, int num_dimensions, typename K=int>
 struct ndarray {
-    ndarray(array<K, num_dimensions> _dimensions, T default_value=0) {
+    T m_default_value;
+    // Suppose dimensions is d1 x d2 x d3 x d4
+    // Then multiplier will be
+    // {d2*d3*d4, d3*d4, d4, 1}
+    array<int, num_dimensions> multiplier;
+    array<int, num_dimensions> dimensions;
+    vector<T> data;
+
+    ndarray(array<K, num_dimensions> _dimensions, T default_value=0) : m_default_value(default_value) {
         FOR(i, 0, num_dimensions - 1) dimensions[i] = _dimensions[i];
         int rolling_mult = 1;
         DOR(i, num_dimensions-1, 0) {
            multiplier[i] = rolling_mult;
            rolling_mult *= dimensions[i];
         }
-        data = vector<T>(rolling_mult + 1, default_value);
+        data = vector<T>(rolling_mult, default_value);
     }
 
     template <typename... Indices>
     T& operator() (Indices... args) {
         array<int, num_dimensions> indices = {static_cast<int>(args)...};
         int flatIndex = 0;
-        FOR(i, 0, num_dimensions-1) if(indices[i] < 0 or indices[i] >= dimensions[i]) return data.back();
+        FOR(i, 0, num_dimensions-1) if(indices[i] < 0 or indices[i] >= dimensions[i]) return m_default_value;
         FOR(i, 0, num_dimensions-1) flatIndex += indices[i] * multiplier[i];
         return data[flatIndex];
     }
 
     T sum() {
         T tot(0);
-        FOR(i, 0, len(data) - 2) tot += data[i];
+        FOR(i, 0, len(data) - 1) tot += data[i];
         return tot;
     }
 
-    array<int, num_dimensions> multiplier;
-    array<int, num_dimensions> dimensions;
-    vector<T> data;
-
     friend std::istream& operator>>(std::istream& os, ndarray& arr) {
-        FOR(i, 0, len(arr.data) - 2) os >> arr.data[i];
+        FOR(i, 0, len(arr.data) - 1) os >> arr.data[i];
         return os;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const ndarray& arr) {
         // For a 1D array, just print the array
         if (num_dimensions == 1) {
-            FOR(i, 0, len(arr.data) - 2) {
+            FOR(i, 0, len(arr.data) - 1) {
                 os << arr.data[i];
-                if (i < len(arr.data) - 2) os << " ";
+                if (i < len(arr.data) - 1) os << " ";
             }
             return os;
         }
         // Unfold the last dimension of the array but key everything else
-        FOR(i, 0, len(arr.data) - 2) {
-            if (i % arr.multiplier[len(arr.multiplier) - 2] == 0) {
+        FOR(i, 0, len(arr.data) - 1) {
+            int last_dim_size = arr.multiplier[len(arr.multiplier) - 2];
+            if (i % last_dim_size == 0) {
                 // Print the index of this row
                 ll idx = i;
                 FOR(j, 0, num_dimensions - 2) {
@@ -69,9 +74,8 @@ struct ndarray {
             }
             // Print out the item itself
             os << arr.data[i] << " ";
-            if (i % arr.multiplier[len(arr.multiplier) - 2] == arr.multiplier[len(arr.multiplier) - 2] - 1) {
+            if (i % last_dim_size == last_dim_size - 1)
                 os << "\n";
-            }
         }
         return os;
     }
