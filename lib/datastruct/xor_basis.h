b8ebe2f77b2b5b62a314cbbd5a2db5291fdbccc1
jonathanxia <jxia934@gmail.com>
2024-06-22 01:07:21 -0400

    xor_basis.h: Swap out rado's with neal's

--- lib/datastruct/xor_basis.h
+++ lib/datastruct/xor_basis.h
@@ -5,89 +5,89 @@
  * Maintains a basis in XOR space. Given a bunch of integers
  * a[0], a[1], ..., a[n], query what sorts of numbers
  * can be expressed as an XOR of some subset.
+ * 
+ * Source: neal
 */
-template<class T, int max_log=31>
+template<typename T, int BITS=30>
 struct XORBasis {
-    array<T, max_log> base;
-
-    XORBasis() {
-        init();
+    // A list of basis values sorted in decreasing order, where each value has a unique highest bit.
+    // We use a static array instead of a vector for better performance.
+    array<T, BITS> basis;
+    int n = 0;
+ 
+    T min_value(T start) const {
+        if (n == BITS)
+            return 0;
+ 
+        for (int i = 0; i < n; i++)
+            start = min(start, start ^ basis[i]);
+ 
+        return start;
     }
  
+    T max_value(T start = 0) const {
+        if (n == BITS)
+            return (T(1) << BITS) - 1;
+ 
+        for (int i = 0; i < n; i++)
+            start = max(start, start ^ basis[i]);
+ 
+        return start;
+    }
+ 
+    bool add(T x) {
+        x = min_value(x);
+ 
+        if (x == 0)
+            return false;
+ 
+        basis[n++] = x;
+        int k = n - 1;
+ 
+        // Insertion sort.
+        while (k > 0 && basis[k] > basis[k - 1]) {
+            swap(basis[k], basis[k - 1]);
+            k--;
+        }
+ 
+        // Optional: remove the highest bit of x from other basis elements.
+        // for (int i = k - 1; i >= 0; i--)
+        //     basis[i] = min(basis[i], basis[i] ^ x);
+ 
+        return true;
+    }
+
     void init() {
-        base.fill(0);
-    }
- 
-    void add(T val) {
-        for(int i = max_log - 1; i >= 0; i--) {
-            if((val >> i) & 1) {
-                if(!base[i]) {
-                    base[i] = val;
-                    return;
-                } else {
-                    val ^= base[i];
-                }
-            }
-        }
-    }
- 
-    inline int size() const {
-        int sz = 0;
-        for(int i = 0; i < max_log; i++) {
-            sz += (bool)(base[i]);
-        }
-        return sz;
-    }
- 
-    T max_xor() const {
-        T res = 0;
-        for(int i = max_log - 1; i >= 0; i--) {
-            if(!((res >> i) & 1) && base[i]) {
-                res ^= base[i];
-            }
-        }
- 
-        return res;
-    }
- 
-    bool can_create(T val) const {
-        for(int i = max_log - 1; i >= 0; i--) {
-            if(((val >> i) & 1) && base[i]) {
-                val ^= base[i];
-            }
-        }
- 
-        return (val == 0);
-    }
- 
-    vector<T> get_basis() const {
-        vector<T> res;
-        for(int i = 0; i < max_log; i++) {
-            if(base[i]) {
-                res.push_back(base[i]);
-            }
-        }
-        return res;
-    }
- 
-    XORBasis<T> merge(XORBasis<T> other) const {
-        if(max_log < other.max_log) {
-            return other.merge(*this);
-        }
- 
-        XORBasis<T> res = *this;
-        for(auto x: other.base) {
-            if(x) {
-                res.add(x);
-            }
-        }
-        return res;
+        FOR(i, 0, n - 1) basis[i] = 0;
+        n = 0;
     }
 
-    friend ostream& operator<<(ostream& os, const XORBasis x) {
-        os << "XORBasis{";
-        os << x.get_basis() << "}";
+    XORBasis() { init(); }
+    int size() { return n; }
+
+    bool can_create(T x) { return min_value(x) == 0; }
+ 
+    void merge(const XORBasis<T, BITS> &other) {
+        for (int i = 0; i < other.n && n < BITS; i++)
+            add(other.basis[i]);
+    }
+ 
+    void merge(const XORBasis<T, BITS> &a, const XORBasis<T, BITS> &b) {
+        if (a.n > b.n) {
+            *this = a;
+            merge(b);
+        } else {
+            *this = b;
+            merge(a);
+        }
+    }
+
+    friend ostream& operator<<(ostream& os, const XORBasis& p) {
+        os << "XORBasis{ ";
+        FOR(i, 0, p.n - 1) {
+            os << p.basis[i] << " ";
+        }
+        os << "}";
         return os;
     }
 };
- 
