36dd2f9191d03b24f483b29541dab072325724b9
Jonathan Xia <jxia934@gmail.com>
2024-06-27 19:43:16 -0400

    solutions: add my own solutions

---
+++ solutions/jonathan/F_Non_academic_Problem.cpp
@@ -0,0 +1,38 @@
+// Link: https://codeforces.com/contest/1986/problem/F
+#include <lib/vv.h>
+#include <lib/graph/bridges.h>
+#include <lib/dfs.h>
+using namespace vv;
+
+void solve() {
+    ll n, m; cin >> n >> m;
+    vvpl graph(n);
+    cepeat(m) {
+        ll u, v; cin >> u >> v; u--; v--;
+        graph[u].pb({v, 1});
+        graph[v].pb({u, 1});
+    }
+    vpl bridges = graph::find_bridges(graph);
+    dbg(bridges);
+
+    // get the tree
+    DfsTree tree(graph, 0);
+    ll ans = n * (n - 1) / 2;
+    foreachp(u, v, bridges) {
+        if (tree.height[u] > tree.height[v]) {
+            swap(u, v);
+        }
+
+        ll k = tree.num_desc[v];
+        ll j = n - k;
+        if (chkmin(ans, k * (k - 1) / 2 + j * (j - 1) / 2)) dprint("Splitting edge", u + 1, v + 1);
+    }
+    print(ans);
+}
+
+int main() {
+    init();
+    int t; cin >> t;
+    cep(t) solve();
+    return 0;
+}
---
+++ solutions/jonathan/F_Remove_Bridges.cpp
@@ -0,0 +1,66 @@
+// Link: https://codeforces.com/contest/1976/problem/F
+#include <lib/vv.h>
+#include <lib/dfs.h>
+#include <atcoder/lazysegtree>
+using namespace vv;
+
+using S = pl;
+S op(S l, S r) {
+    return max(l, r);
+}
+S e() { return {-1e9, -1}; }
+using F = ll;
+S mapping(F l, S r) {
+    if (!l) return r;
+    return {l + r.first, r.second};
+}
+F composition(F l, F r) { return l + r; }
+F id() { return 0; }
+// atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> seg(a);
+
+void solve() {
+    ll n; cin >> n;
+    vvpl graph(n); inp::read_tree(graph);
+
+    DfsTree tree(graph, 0);
+
+    ll num_edges = n - 1;
+    vl output(n - 1);
+    vpl depths(n);
+    FOR(node, 0, n - 1) {
+        ll c = tree.node_to_counter[node];
+        depths[c] = {tree.height[node], node};
+    }
+
+    atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> seg(depths);
+
+    FOR(k, 0, n - 2) {
+        if (num_edges == 0) break;
+        pl data = seg.query(0, n - 1);
+        num_edges -= data.first;
+        output[k] = num_edges;
+
+        // update it now properly
+        ll node = data.second;
+        FOR(i, 0, data.first - 1) {
+            ll start = tree.node_to_counter[node];
+            ll end = start + tree.num_desc[node] - 1;
+            seg.update(start, end, -1);
+            node = tree.parent[node];
+        }
+    }
+
+    vl ans(n - 1);
+    ans[0] = output[0];
+    SFOR(i, 2, n - 2, 2) {
+        ans[i / 2] = output[i];
+    }
+    print(ans);
+}
+
+int main() {
+    init();
+    int t; cin >> t;
+    cep(t) solve();
+    return 0;
+}
