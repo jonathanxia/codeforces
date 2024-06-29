df3a832e73bcca3983c3508e2ab75b22f3e46875
Jonathan Xia <jxia934@gmail.com>
2024-06-29 15:27:31 -0400

    ndarray.h: Allow negative indexing

--- lib/datastruct/ndarray.h
+++ lib/datastruct/ndarray.h
@@ -1,19 +1,17 @@
 #pragma once
 #include <lib/common.h>
 
-/**
- * arbitrarily high dimension array
- * by default will return a default_value on OOB access
- *
- * Example usage:
- *
- * ndarray<ll> arr({3, 5, 2}, 27);
- * arr(2, 2, 0) = 8;
- * arr(-1, -1, -1) // Gives 27
- * 
- * CHECK_DEFAULT_VALUE means that we reset the default value to the
- * right thing each time. This is safer but costlier.
-*/
+//  arbitrarily high dimension array
+//  by default will return a default_value on OOB access
+//
+//  Example usage:
+//
+//  ndarray<ll> arr({3, 5, 2}, 27);
+//  arr(2, 2, 0) = 8;
+//  arr(-1, -1, -1) // Gives 27
+// 
+//  CHECK_DEFAULT_VALUE means that we reset the default value to the
+//  right thing each time. This is safer but costlier.
 template<typename T, int num_dimensions, bool CHECK_DEFAULT_VALUE=true>
 struct ndarray {
     T m_default_value;
@@ -25,9 +23,12 @@ struct ndarray {
     // {d2*d3*d4, d3*d4, d4, 1}
     array<int, num_dimensions> multiplier;
     array<int, num_dimensions> dimensions;
+    array<int, num_dimensions> base_index;
     vector<T> data;
 
-    ndarray(array<ll, num_dimensions> _dimensions, T default_value=0) : m_default_value(default_value), saved_default_value(default_value) {
+    ndarray(array<ll, num_dimensions> _dimensions, T default_value=0)
+        : m_default_value(default_value), saved_default_value(default_value)
+    {
         FOR(i, 0, num_dimensions - 1) dimensions[i] = _dimensions[i];
         int rolling_mult = 1;
         DOR(i, num_dimensions-1, 0) {
@@ -35,11 +36,28 @@ struct ndarray {
            rolling_mult *= dimensions[i];
         }
         data = vector<T>(rolling_mult, default_value);
+        FOR(i, 0, num_dimensions - 1) base_index[i] = 0;
     }
+
+    ndarray(array<ll, num_dimensions> base_index_, array<ll, num_dimensions> _dimensions, T default_value=0)
+        : m_default_value(default_value), saved_default_value(default_value)
+    {
+        FOR(i, 0, num_dimensions - 1) dimensions[i] = _dimensions[i] - base_index_[i];
+        int rolling_mult = 1;
+        DOR(i, num_dimensions-1, 0) {
+           multiplier[i] = rolling_mult;
+           rolling_mult *= dimensions[i];
+        }
+        data = vector<T>(rolling_mult, default_value);
+        FOR(i, 0, num_dimensions - 1) base_index[i] = base_index_[i];
+    }
+
+
 
     template <typename... Indices>
     T& operator() (Indices... args) {
         array<int, num_dimensions> indices = {static_cast<int>(args)...};
+        FOR(i, 0, num_dimensions - 1) indices[i] -= base_index[i];
         int flatIndex = 0;
 
         FOR(i, 0, num_dimensions-1) if(indices[i] < 0 or indices[i] >= dimensions[i]) {
@@ -77,7 +95,7 @@ struct ndarray {
                 // Print the index of this row
                 ll idx = i;
                 FOR(j, 0, num_dimensions - 2) {
-                    os << idx / arr.multiplier[j] << " ";
+                    os << idx / arr.multiplier[j] + arr.base_index[j] << " ";
                     idx = idx % arr.multiplier[j];
                 }
                 os << ": ";
