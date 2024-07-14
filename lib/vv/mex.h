8cd8567e6b4ad04f175c61ff3aef42c3871f45f4
Jonathan Xia <jxia934@gmail.com>
2024-07-14 18:58:15 -0400

    Revert "mex.h: Compute new mex version"
    This reverts commit de285bd8453d2dbae9c1f79eb7e7ff7ab2063949.

--- lib/vv/mex.h
+++ lib/vv/mex.h
@@ -2,26 +2,18 @@
 #include <lib/common.h>
 
 namespace vv {
-    // Computes mex(a)
-    // :param positive: If true, then finds the first positive number not in a
-    //     otherwise it is the first non-negative number not in a
-    // :param k: Computes the k-th non-negative number not in a
-    //     (or k-th positive number if positive is true)
 template <typename T>
-ll mex(const vector<T>& a, bool positive=false, int k=1) {
+ll mex(const vector<T>& a, bool positive=false) {
     ll n = len(a);
     ll start = int(positive);
-    vb exists(start + n + k + 1);
+    vb exists(start + n);
     FOR(i, 0, n - 1) {
-        if (a[i] >= len(exists)) continue;
+        if (a[i] >= n + start) continue;
         exists[a[i]] = true;
     }
-    ll num_seen = 0;
-    ll i = start - 1;
-    do {
-        i++;
-        if (!exists[i]) num_seen++;
-    } while (num_seen < k);
-    return i;
+    FOR(i, start, n - 1 + start) {
+        if (!exists[i]) return i;
+    }
+    return n + start;
 }
 }
