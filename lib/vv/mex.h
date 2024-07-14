de285bd8453d2dbae9c1f79eb7e7ff7ab2063949
jonathanxia <jxia934@gmail.com>
2024-07-13 21:00:02 -0400

    mex.h: Compute new mex version

--- lib/vv/mex.h
+++ lib/vv/mex.h
@@ -2,18 +2,26 @@
 #include <lib/common.h>
 
 namespace vv {
+    // Computes mex(a)
+    // :param positive: If true, then finds the first positive number not in a
+    //     otherwise it is the first non-negative number not in a
+    // :param k: Computes the k-th non-negative number not in a
+    //     (or k-th positive number if positive is true)
 template <typename T>
-ll mex(const vector<T>& a, bool positive=false) {
+ll mex(const vector<T>& a, bool positive=false, int k=1) {
     ll n = len(a);
     ll start = int(positive);
-    vb exists(start + n);
+    vb exists(start + n + k + 1);
     FOR(i, 0, n - 1) {
-        if (a[i] >= n + start) continue;
+        if (a[i] >= len(exists)) continue;
         exists[a[i]] = true;
     }
-    FOR(i, start, n - 1 + start) {
-        if (!exists[i]) return i;
-    }
-    return n + start;
+    ll num_seen = 0;
+    ll i = start - 1;
+    do {
+        i++;
+        if (!exists[i]) num_seen++;
+    } while (num_seen < k);
+    return i;
 }
 }
