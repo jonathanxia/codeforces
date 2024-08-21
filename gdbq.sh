f88564e2ea7eb5c0add1d72798bb119d6f47656b
Jonathan Xia <jxia934@gmail.com>
2024-08-20 20:27:23 -0400

    gdbq.sh: fix gdbq.sh

--- gdbq.sh
+++ gdbq.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-g++ -g -Wno-return-type -Wshadow -std=c++17 -D_GLIBCXX_DEBUG -fsanitize=undefined,address -ftrapv $1
+g++ -g -Wno-return-type -Wshadow -O0 -std=c++17 -D_GLIBCXX_DEBUG -fsanitize=undefined,address -ftrapv -DDEBUG -I $(dirname $(realpath $0)) $1
 python3 $(dirname $(realpath $0))'/cph_to_txt.py' -i temp.txt $1 >/dev/null $2
 gdb --quiet -ex='r <temp.txt' a.out
 rm temp.txt
