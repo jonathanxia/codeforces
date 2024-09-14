6f2149e52741feee752fa14cf418b9fe0b753f7d
Jonathan Xia <jxia934@gmail.com>
2024-09-13 22:19:31 -0400

    vv/counter.h: Add version for strings

--- lib/vv/counter.h
+++ lib/vv/counter.h
@@ -2,22 +2,29 @@
 #include <lib/common.h>
 
 namespace vv {
-template <typename T>
-unordered_map<T, ll, custom_hash> counter(const vector<T>& a, ll start = 0, ll end = -1)
-{
-    if (start < 0) {
-        start += len(a);
-    }
-    if (end < 0) {
-        end += len(a);
-    }
-
-    unordered_map<T, ll, custom_hash> result;
-    FOR(i, start, end)
+    template <typename T>
+    unordered_map<T, ll, custom_hash> counter(const vector<T>& a, ll start = 0, ll end = -1)
     {
-        result[a[i]]++;
+        if (start < 0) {
+            start += len(a);
+        }
+        if (end < 0) {
+            end += len(a);
+        }
+
+        unordered_map<T, ll, custom_hash> result;
+        FOR(i, start, end)
+        {
+            result[a[i]]++;
+        }
+
+        return result;
     }
 
-    return result;
-}
+    umap<char, ll> counter(const string& s)
+    {
+        umap<char, ll> result;
+        FOR(i, 0, len(s) - 1) result[s[i]]++;
+        return result;
+    }
 }
