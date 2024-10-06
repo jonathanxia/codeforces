f95c52233c88e9e1d7720d50fa95021314beb52e
Kevin1729 <kxia1729@gmail.com>
2024-10-06 15:03:13 -0700

    lib/vv/unique.h: add a stable dedupe function

--- lib/vv/unique.h
+++ lib/vv/unique.h
@@ -8,4 +8,22 @@ std::vector<T> unique(const std::vector<T>& input) {
     std::vector<T> uniqueVector(uniqueSet.begin(), uniqueSet.end());
     return uniqueVector;
 }
+
+// Keeps deduped items in the same order in which they appear in input
+// ie. if the first copy of T1 is before the first copy of T2 in input,
+// then T1 will be before T2 in the output
+template <typename T>
+std::vector<T> stable_unique(const std::vector<T>& input) {
+    std::vector<T> ans;
+    uset<T> already_found;
+    foreach (item, input) {
+        if (already_found.count(item)) {
+            continue;
+        } else {
+            ans.pb(item);
+            already_found.insert(item);
+        }
+    }
+    return ans;
+}
 }
