8c9c48cb40c095f9945fdf74af85b1c778caa594
jonathanxia <jxia934@gmail.com>
2024-11-02 20:59:10 -0400

    prefix_xor.h: Make algorithm simpler

--- lib/bitster/prefix_xor.h
+++ lib/bitster/prefix_xor.h
@@ -3,25 +3,15 @@
 namespace bitster
 {
     // computes 1 ^ 2 ^ 3 ^ ... ^ num in constant time
-    ll prefix_xor(ll num)
-    {
-        ll prefix = 0;
-        rep(i, 0, 62)
-        {
-            ll mask = 1LL << i;
-            if (i == 0) {
-                // special case
-                ll mod4 = num & 0b11;
-                if (mod4 == 1 || mod4 == 2) {
-                    prefix |= mask;
-                }
-            } else {
-                ll mod = num & ((mask << 1) - 1);
-                if (mod >= mask && !((mod - mask) & 1)) {
-                    prefix |= mask;
-                }
-            }
-        }
-        return prefix;
+    ll prefix_xor(ll num) {
+        // Key observation, every four numbers are 0 XOR, so we only look
+        // at it mod 4.
+        ll ans = 0;
+        if ((num & 1) == 0) ans |= ((num >> 2) << 2);
+        ll rem = num & 3; // Take this number (mod 4)
+        if (rem == 0 || rem == 3) return ans;
+        if (rem == 1) return ans | 1;
+        if (rem == 2) return ans | 3;
+        return 0;
     }
 } // namespace bitster
