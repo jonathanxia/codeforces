1227d78f4bf88d5f4daaf70494e8d02893cd949d
Kevin1729 <kxia1729@gmail.com>
2024-11-16 23:18:52 -0800

    kactl/number-theory/ModularArithmetic.h: tfw constructor does not apply the mod

--- lib/kactl/number-theory/ModularArithmetic.h
+++ lib/kactl/number-theory/ModularArithmetic.h
@@ -6,6 +6,8 @@
  * Source: folklore
  * Description: Operators for modular arithmetic. You need to set {\tt mod} to
  * some number first and then you can use the structure.
+ * Edit by Kevin Xia: The original constructor for Mod didn't apply % mod to xx.
+ * This means that the invariant that x be between [0, mod) is violated.
  */
 #include <lib/common.h>
 
@@ -15,7 +17,7 @@ namespace kactl {
 const ll mod = 17; // change to something else
 struct Mod {
 	ll x;
-	Mod(ll xx) : x(xx) {}
+	Mod(ll xx) : x(((xx % mod) + mod) % mod) {}
 	Mod operator+(Mod b) { return Mod((x + b.x) % mod); }
 	Mod operator-(Mod b) { return Mod((x - b.x + mod) % mod); }
 	Mod operator*(Mod b) { return Mod((x * b.x) % mod); }
