9babe52d8693ceafd8e1e62d30b2ae451d7f90f1
Kevin1729 <kxia1729@gmail.com>
2024-11-16 23:28:35 -0800

    lib/kactl/numerical/SolveLinear.h: pseudo-templatize for compatibility with Mod

--- lib/kactl/numerical/SolveLinear.h
+++ lib/kactl/numerical/SolveLinear.h
@@ -5,27 +5,41 @@
  * License: CC0
  * Description: Solves $A * x = b$. If there are multiple solutions, an arbitrary one is returned.
  *  Returns rank, or -1 if no solutions. Data in $A$ and $b$ is lost.
+ * Edit by Kevin Xia: pseudo-templatized to be compatible with Mod
  * Time: O(n^2 m)
  * Status: tested on kattis:equationsolver, and bruteforce-tested mod 3 and 5 for n,m <= 3
  */
 #include <lib/common.h>
 
 namespace kactl {
-typedef vector<double> vd;
-const double eps = 1e-12;
+typedef double T; // change to whatever type you need
+typedef vector<T> vT;
 
-int solveLinear(vector<vd>& A, vd& b, vd& x) {
+bool is_zero(T num){
+	const double eps = 1e-12;
+	return fabs(num) <= eps;
+}
+
+// If field T does not support operator>, just return !is_zero(b).
+// Mainly used to prevent numerical instability
+bool is_greater(T a, T b){
+	return fabs(a) > fabs(b);
+}
+
+int solveLinear(vector<vT>& A, vT& b, vT& x) {
 	int n = len(A), m = len(x), rank = 0, br, bc;
 	if (n) assert(len(A[0]) == m);
 	vi col(m); iota(ALL(col), 0);
 
 	rep(i,0,n) {
-		double v, bv = 0;
-		rep(r,i,n) rep(c,i,m)
-			if ((v = fabs(A[r][c])) > bv)
+		T v, bv = 0;
+		rep(r,i,n) rep(c,i,m) {
+			v = fabs(A[r][c]);
+			if (is_greater(v, bv))
 				br = r, bc = c, bv = v;
-		if (bv <= eps) {
-			rep(j,i,n) if (fabs(b[j]) > eps) return -1;
+		}
+		if (is_zero(bv)) {
+			rep(j,i,n) if (!is_zero(b[j])) return -1;
 			break;
 		}
 		swap(A[i], A[br]);
@@ -41,7 +55,8 @@ int solveLinear(vector<vd>& A, vd& b, vd& x) {
 		rank++;
 	}
 
-	x.assign(m, 0);
+	// Erase this for loop part if you only care about the rank.
+	x.assign(m, T(0));
 	for (int i = rank; i--;) {
 		b[i] /= A[i][i];
 		x[col[i]] = b[i];
